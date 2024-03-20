006e73b01ad370d01a9258b908df57f41ad21b93
Mark Dietrich <MarkADietr@gmail.com>
2024-03-20 18:40:31 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+logo_img = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100,100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1, sticky="e")
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_user = Label(text="Email/Username:")
+email_user.grid(column=0, row=2, sticky="e")
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3, sticky="e")
+
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+password_btn = Button(text="Generate Password")
+password_btn.grid(column=2, row=3)
+
+add_btn = Button(width=36, text="Add")
+add_btn.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
